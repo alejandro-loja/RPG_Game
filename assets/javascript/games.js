623a924fe928d44cc995b9380e26e76b17330070
Alejandro Loja <alejandro.loja@gmail.com>
2019-09-05 22:29:16 -0400

    Add new render function

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,37 +1,83 @@
 $(document).ready(function () {
     //READY TO PLAY
     console.log("jQuery Ready!");
-    var wins = 0;
-    var losses = 0;
-    var genNumber;
-    var totalVal = 0;
+    let wins = 0;
+    let losses = 0;
+    let genNumber;
+    let totalVal = 0;
+
+    let pokemonObj =
+    {
+        bulbasaur: {
+            name: 'Bulbasaur',
+            kdex: 1,
+            type: 'grass',
+            hp: 60,
+            attack1: 30,
+            attack2: 20
+        },
+        charmander: {
+            name: 'Charmander',
+            kdex: 3,
+            type: 'fire',
+            hp: 65,
+            attack1: 10,
+            attack2: 40
+        },
+        squirtle: {
+            name: 'Squirtle',
+            kdex: 4,
+            type: 'water',
+            hp: 50,
+            attack1: 20,
+            attack2: 25
+        }
+    };
+
+    const pkmnToRender = [pokemonObj.bulbasaur.name, pokemonObj.charmander.name, pokemonObj.squirtle.name]
+
+    //used to create pokmeon based on name of pokemon
+    function jqCreateImgPkmn(name) {
+        let pkmnImg = $(`<img src="assets/images/${name}.png" id="${name}" class="pokemon animated bounce faster" alt="${name}">`);
+        return pkmnImg;
+    }
+    //centers pokemon for the first screen ie choose a pokemon
+    function renderChoosePokemon(arr) {
+        arr.map(name => $('#center-stage1').append(jqCreateImgPkmn(name)));
+
+    };
 
     function pokemonChosen() {
         $('.pokemon').on('mouseover', function () {
-            var whichPokemon = $(this).attr("id");
-            console.log('The pokemon is ' + whichPokemon)
+            let whichPokemon = $(this).attr("id");
+
+            // todo for adding text show stats of pokemon.. i guess.
+            $('#pokemon-chosen').text(whichPokemon)
+            console.log('The pokemon is ' + whichPokemon);
             $('#' + whichPokemon).addClass('infinite');
-            // var crystIntValue = parseInt(crystValue);
-            // totalVal = totalVal + crystIntValue;
-            // $('#total-value').text(totalVal);
-            // winnerORloser();
         });
     };
+
+    function pokemonChosenMouseOut() {
+        $('.pokemon').on('mouseout', function () {
+            let whichPokemon = $(this).attr("id");
+            console.log('The pokemon is ' + whichPokemon + ' Mouseout');
+            $('#' + whichPokemon).removeClass('infinite');
+        });
+    };
+
     function pokemonChosenClick() {
         $('.pokemon').on('click', function () {
             console.log('clicked')
-            var whichPokemon = $(this).attr("id");
-
-            // var whichPokemon = $(this).attr("id");
-            // console.log('The pokemon is ' + whichPokemon)
+            let whichPokemon = $(this).attr("id");
             $('#' + whichPokemon).removeClass('infinite');
-            // var crystIntValue = parseInt(crystValue);
-            // totalVal = totalVal + crystIntValue;
-            // $('#total-value').text(totalVal);
-            // winnerORloser();
+            $('#pokemon-chosen').text(`You chose ${whichPokemon}!`)
+            $('body').addClass('fadeOutRightBig animated fast');
         });
     };
 
+    renderChoosePokemon(pkmnToRender);
     pokemonChosen();
+    pokemonChosenMouseOut();
     pokemonChosenClick();
 });
